cfbb0d20153c73ba3a930421089c14d090592933
Viktor <ctxx61@gmail.com>
2018-06-15 16:32:07 +0100

    Complete the cast example

--- C++_project1/C++_project1/casting.cpp
+++ C++_project1/C++_project1/casting.cpp
@@ -12,7 +12,30 @@ void main()
 	Tweeter lihuacons("Li", "Hua", 456, "@lihuaCN");
 	Person* p = &lihuacons;
 	//Tweeter* pt = (Tweeter*)p; C Style cast
-	Tweeter* pt = static_cast<Tweeter*>(p);
+	Tweeter* pt = static_cast<Tweeter*>(p); // static cast example, when you are sure which type the pointer points to, use static_cast
+	cout << pt->GetName() << endl;
 
+	//Example of dynamic_cast
+	Resource r("local");
+	Tweeter *pt2;
+	cout << "Use Resource or Tweeter?" << endl;
+	std::string answer;
+	cin >> answer;
+	if (answer == "r")
+	{
+		pt2 = dynamic_cast<Tweeter*>(&r);
+	}
+	else
+	{
+		pt2 = dynamic_cast<Tweeter*>(p);
+	}
 
+	if (pt2)
+	{
+		cout << pt2->GetName() << endl;
+	}
+	else
+	{
+		cout << "Null" << endl;
+	}
 }
